cb63288c7b755c9404b415a967310bef1b99eb83
Sean Moore <sean.moore@animatron.com>
2026-06-16 10:47:42 +0300

    fix part 2 (#6)

--- welley/tests/template.spec.ts
+++ welley/tests/template.spec.ts
@@ -1,11 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test.describe(() => {
+    test.beforeEach(async ({ page }) => {
+        // Go to the starting url before each test.
+        await page.goto('localhost:3000/');
+    });
 
     test('your test name here', async ({ page }) => {
         // your code here
         // await page.locator('your locator here').click(); 
-        
+
         // await expect(page.locator('your locator here')).toBeVisible();
     });
 
